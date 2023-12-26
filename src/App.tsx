06f481a8151fdb74018fe05fdc8c3c5813862fac
sinofseven <em.s.00001@gmail.com>
2023-12-27 03:05:26 +0900

    first impl

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [filenames, setFilenames] = useState<string[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+      const users = await resp.json();
+      const names = users.map((item: any) => item.name as string);
+      console.log(names);
+      setFilenames(names);
+    };
+    if (loading) {
+      setLoading(false);
+      getUsers();
+    }
+  }, [filenames, loading]);
+
+  return (
+    <>
+      <div className="container">
+        <h1>test</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
